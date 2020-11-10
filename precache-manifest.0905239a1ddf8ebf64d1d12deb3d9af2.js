62e1d022bb82c01b12b1b990405832aab30b635e
Chinito4852 <tristinowong@gmail.com>
2020-11-09 18:19:41 -0800

    Updates

---
+++ precache-manifest.0905239a1ddf8ebf64d1d12deb3d9af2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae85d64a7f824d20f91ec7eeba1e212e",
+    "url": "/Gnews/index.html"
+  },
+  {
+    "revision": "f821f091a2dbcd8e716a",
+    "url": "/Gnews/static/css/main.34045bc2.chunk.css"
+  },
+  {
+    "revision": "70ce35770eeb20d98514",
+    "url": "/Gnews/static/js/2.4dc5ef8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gnews/static/js/2.4dc5ef8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f821f091a2dbcd8e716a",
+    "url": "/Gnews/static/js/main.76215d8f.chunk.js"
+  },
+  {
+    "revision": "d1ce04f057027ce52522",
+    "url": "/Gnews/static/js/runtime-main.db7d5046.js"
+  }
+]);
